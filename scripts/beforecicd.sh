8df771325bfd154d646514b688e7a017dea376f7
khansuhl <khansuhl@gmail.com>
2018-07-10 16:26:54 +0530

    modified shell file

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -5,10 +5,10 @@ sudo su -c "apt-get update"
 sudo su -c "service codedeploy-agent start"
 
 KEY="$(aws s3 ls s3://jenkins-code-deploy-integration --recursive --region ap-south-1 | sort | tail -n 1 | awk '{print $4}')"
-sudo su -c "aws s3 cp s3://jenkins-code-deploy-integration/$KEY /home/ubuntu/fundooNotes.zip --region ap-south-1"
+sudo su -c "aws s3 cp s3://jenkins-code-deploy-integration/$KEY /home/ubuntu/$KEY --region ap-south-1"
 
 sudo su -c "mkdir /home/ubuntu/fundooNotes-microservices"
-sudo su -c "unzip -o /home/ubuntu/fundooNotes.zip -d /home/ubuntu/fundooNotes-microservices"
+sudo su -c "unzip -o /home/ubuntu/$KEY -d /home/ubuntu/fundooNotes-microservices"
 sudo su -c "cd /home/ubuntu/fundooNotes-microservices/target;
 	    mv springboot-helloworld-jar-code-deploy-0.0.1-SNAPSHOT.jar /home/ubuntu/hello.jar;
 	    java -jar /home/ubuntu/hello.jar"
