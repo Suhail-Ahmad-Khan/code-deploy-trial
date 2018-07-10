a46d9c358501ecfcfb1bbdcc8f7cc13aaab05f79
khansuhl <khansuhl@gmail.com>
2018-07-10 16:09:49 +0530

    corrected beforecicd.sh file

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -8,8 +8,8 @@ KEY="$(aws s3 ls s3://jenkins-code-deploy-integration --recursive --region ap-so
 sudo su -c "aws s3 cp s3://jenkins-code-deploy-integration/$KEY /home/ubuntu/fundooNotes.zip --region ap-south-1"
 
 sudo su -c "mkdir /home/ubuntu/fundooNotes-microservices"
-sudo su -c "unzip -o /home/ubuntu/fundooNotes.zip -d /home/ubuntu/fundooNotes"
-sudo su -c "cd /home/ubuntu/fundooNotes/target;
+sudo su -c "unzip -o /home/ubuntu/fundooNotes.zip -d /home/ubuntu/fundooNotes-microservices"
+sudo su -c "cd /home/ubuntu/fundooNotes-microservices/target;
 	    mv springboot-helloworld-jar-code-deploy-0.0.1-SNAPSHOT.jar /home/ubuntu/hello.jar;
 	    java -jar /home/ubuntu/hello.jar"
 
