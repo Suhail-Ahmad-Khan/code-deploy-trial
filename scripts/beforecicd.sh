af3afa0b383cbf217e69a975c1053272b2dc5061
khansuhl <khansuhl@gmail.com>
2018-07-10 23:29:18 +0530

    new modifications

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -3,10 +3,10 @@
 sudo su -c "apt-get install unzip"
 sudo su -c "apt-get update"
 sudo su -c "service codedeploy-agent start"
-sudo su -c "cd /home/ubuntu/fundooNotes-microservices;
-	    ./stopjar.sh"
-#sudo su -c "pid=`ps aux | grep hello.jar | awk '{print $2}'`;
-#	    kill -9 $pid"
+#sudo su -c "cd /home/ubuntu/fundooNotes-microservices;
+#	    ./stopjar.sh"
+pid="$(`ps aux | grep hello.jar | awk '{print $2}'`)"
+sudo su -c "kill -9 $pid"
 sudo su -c "rm -rf /home/ubuntu/fundooNotes-microservices;
 	    rm -rf /home/ubuntu/hello.jar"
 
