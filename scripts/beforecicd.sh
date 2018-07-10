b2e54d80c3a9676c1a0e9b32136df4a4558f16f3
khansuhl <khansuhl@gmail.com>
2018-07-10 17:50:57 +0530

    modified script to run in background

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -10,6 +10,5 @@ sudo su -c "aws s3 cp s3://jenkins-code-deploy-integration/$KEY /home/ubuntu/$KE
 sudo su -c "mkdir /home/ubuntu/fundooNotes-microservices"
 sudo su -c "unzip -o /home/ubuntu/$KEY -d /home/ubuntu/fundooNotes-microservices"
 sudo su -c "cd /home/ubuntu/fundooNotes-microservices/target;
-	    mv springboot-helloworld-jar-code-deploy-0.0.1-SNAPSHOT.jar /home/ubuntu/hello.jar;
-	    java -jar /home/ubuntu/hello.jar &"
+	    mv springboot-helloworld-jar-code-deploy-0.0.1-SNAPSHOT.jar /home/ubuntu/hello.jar"
 
