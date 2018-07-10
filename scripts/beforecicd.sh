47e24389490d27b78109e9e0693f085914e99356
khansuhl <khansuhl@gmail.com>
2018-07-10 22:53:23 +0530

    new modifications

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -3,8 +3,8 @@
 sudo su -c "apt-get install unzip"
 sudo su -c "apt-get update"
 sudo su -c "service codedeploy-agent start"
-sudo su -c "cd /home/ubuntu/fundooNotes-microservices;
-	    ./stopjar.sh"
+#sudo su -c "cd /home/ubuntu/fundooNotes-microservices;
+#	    ./stopjar.sh"
 sudo su -c "rm -rf /home/ubuntu/fundooNotes-microservices;
 	    rm -rf /home/ubuntu/hello.jar"
 
@@ -15,7 +15,9 @@ sudo su -c "mkdir /home/ubuntu/fundooNotes-microservices"
 sudo su -c "unzip -o /home/ubuntu/$KEY -d /home/ubuntu/fundooNotes-microservices"
 sudo su -c "rm -rf /home/ubuntu/$KEY"
 sudo su -c "cd /home/ubuntu/fundooNotes-microservices/target;
-	    mv springboot-helloworld-jar-code-deploy-0.0.1-SNAPSHOT.jar /home/ubuntu/hello.jar"
-sudo su -c "cd /home/ubuntu/fundooNotes-microservices;
-	    ./startjar.sh; exit"
+	    mv springboot-helloworld-jar-code-deploy-0.0.1-SNAPSHOT.jar /home/ubuntu/hello.jar;
+	    nohup java -jar /home/ubuntu/hello.jar &;
+	    exit"
+#sudo su -c "cd /home/ubuntu/fundooNotes-microservices;
+#	    ./startjar.sh; exit"
 
